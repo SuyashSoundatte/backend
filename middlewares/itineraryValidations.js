353c4a679aac2997a78534e4f9920ff219c363fd
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-08-31 13:37:16 +0530

    route docs

--- middlewares/itineraryValidations.js
+++ middlewares/itineraryValidations.js
@@ -1,19 +1,21 @@
 // itineraryValidations.js
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
 const itineraryValidations = [
-    body('title').notEmpty().withMessage('Title is required'),
-    body('description').notEmpty().withMessage('Description is required'),
-    body('startDate').isISO8601().withMessage('Start date must be a valid date'),
-    body('endDate').isISO8601().withMessage('End date must be a valid date'),
-    body('destinations').isArray().withMessage('Destinations must be an array'),
+    body("title").notEmpty().withMessage("Title is required"),
+    body("description").notEmpty().withMessage("Description is required"),
+    body("startDate")
+        .isISO8601()
+        .withMessage("Start date must be a valid date"),
+    body("endDate").isISO8601().withMessage("End date must be a valid date"),
+    body("destinations").isArray().withMessage("Destinations must be an array"),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
         next();
-    }
+    },
 ];
 
 module.exports = itineraryValidations;
---
+++ routes.md
@@ -0,0 +1,5 @@
+/ping -> for pinging (get)
+
+/api/auth ->
+/api/auth/signup (post) (status 201 is success)
+/api/auth/login (post) (status 200 is success)
