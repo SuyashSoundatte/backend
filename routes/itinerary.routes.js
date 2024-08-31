884053a2df1c078b9abab87adb787606c35b37d5
Neeraj <neerajsurnis143r@gmail.com>
2024-08-31 09:32:12 +0530

    formatted

--- routes/itinerary.routes.js
+++ routes/itinerary.routes.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { createItinerary, getItinerary, updateItinerary, deleteItinerary } = require('../controllers/itinerary.controller');
+const {
+  createItinerary,
+  getItinerary,
+  updateItinerary,
+  deleteItinerary,
+} = require("../controllers/itinerary.controller");
 
 // Route to create a new itinerary
-router.post('/create', createItinerary);
+router.post("/create", createItinerary);
 
 // Route to get an existing itinerary by ID
-router.get('/get/:id', getItinerary);
+router.get("/get/:id", getItinerary);
 
 // Route to update an existing itinerary by ID
-router.put('/update/:id', updateItinerary);
+router.put("/update/:id", updateItinerary);
 
 // Route to delete an existing itinerary by ID
-router.delete('/delete/:id', deleteItinerary);
+router.delete("/delete/:id", deleteItinerary);
 
 module.exports = router;
